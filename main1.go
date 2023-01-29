3dc37e6d71f3417cbf828de08024b3ac3a25fa5d
a-inara-b <almagambetovai@mail.ru>
2023-01-29 23:13:57 +0600

    second commit

--- main1.go
+++ main1.go
@@ -1,55 +1,55 @@
 // package Index
 
-func FirstOccur(arr []float64, num float64) int {
-	for i := 0; i < len(arr); i++ {
-		if arr[i] == num {
-			return i
-			break
-		}
-	}
-}
+// func FirstOccur(arr []float64, num float64) int {
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] == num {
+// 			return i
+// 			break
+// 		}
+// 	}
+// }
 
-func LastOccur(arr []float64, num float64) int {
-	for i := len(arr) - 1; i >= 0; i-- {
-		if arr[i] == num {
-			return i
-			break
-		}
-	}
-}
+// func LastOccur(arr []float64, num float64) int {
+// 	for i := len(arr) - 1; i >= 0; i-- {
+// 		if arr[i] == num {
+// 			return i
+// 			break
+// 		}
+// 	}
+// }
 
-func Count(arr []float64, num float64) int {
-	cnt := 0
-	for i := 0; i < len(arr); i++ {
-		if arr[i] == num {
-			cnt = cnt + 1
-		}
-	}
-	return cnt
-}
+// func Count(arr []float64, num float64) int {
+// 	cnt := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] == num {
+// 			cnt = cnt + 1
+// 		}
+// 	}
+// 	return cnt
+// }
 
-func Merge(arr1, arr2 []float64) []float64 {
-	var res [len(arr1) + len(arr2)]float64
-	for i := 0; i < len(arr1); i++ {
-		res[i] = arr1[i]
-	}
-	for i := 0; i < len(arr2); i++ {
-		res[i+len(arr1)] = arr2[i]
-	}
-	return res
-}
+// func Merge(arr1, arr2 []float64) []float64 {
+// 	var res [len(arr1) + len(arr2)]float64
+// 	for i := 0; i < len(arr1); i++ {
+// 		res[i] = arr1[i]
+// 	}
+// 	for i := 0; i < len(arr2); i++ {
+// 		res[i+len(arr1)] = arr2[i]
+// 	}
+// 	return res
+// }
 
-func Sort(n []float64) []float64 {
-	var i = 1
-	for i < len(n) {
-		var j = i
-		for j >= 1 && n[j] < n[j-1] {
-			n[j], n[j-1] = n[j-1], n[j]
+// func Sort(n []float64) []float64 {
+// 	var i = 1
+// 	for i < len(n) {
+// 		var j = i
+// 		for j >= 1 && n[j] < n[j-1] {
+// 			n[j], n[j-1] = n[j-1], n[j]
 
-			j--
-		}
+// 			j--
+// 		}
 
-		i++
-	}
-	return n
-}
+// 		i++
+// 	}
+// 	return n
+// }
